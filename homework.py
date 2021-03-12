1b94ce557d76930d0f680fdc1911348bab146153
Dmitriy <fable1905@yandex.ru>
2021-03-12 18:56:40 +0300

    'сделал'

--- homework.py
+++ homework.py
@@ -1,9 +1,10 @@
 import os
 import time
 
+import logging
 import requests
-import telegram
 from dotenv import load_dotenv
+from telegram import Bot
 
 load_dotenv()
 
@@ -14,8 +15,8 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected' and homework['status'] != 'reviewing':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -23,27 +24,38 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    from_date = {'from_date': current_timestamp}
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    response = requests.get(url, params=from_date, headers=headers)
+    homework_statuses = response
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
+        filename='setup.cfg', filemode='w')
+    logging.debug('Запущено')
+    logging.info('Сообщение отправлено')
+    logging.warning('Большая нагрузка, хелп')
+    logging.error('Бот не смог отправить сообщение')
+    logging.critical('Всё упало! Зовите админа!1!111')
+    return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    bot_client = Bot(token=TELEGRAM_TOKEN)
+    current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            current_timestamp = new_homework.get('current_date', current_timestamp)
+            time.sleep(300)
 
         except Exception as e:
             print(f'Бот столкнулся с ошибкой: {e}')
